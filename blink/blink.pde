ab5069f6418eb940fcd1b77251a3fe59a49e3535
Alberto Morales <amorales@opnet.com>
2011-09-25 11:08:43 -0400

    arduino blink

---
+++ blink/blink.pde
@@ -0,0 +1,17 @@
+//This example shows how to do the simple blink with C++ classes.
+//Create a tab called led.h
+//In that tab you will define the Led class
+//Add the #include "led.h" in this main file, so that it is available
+
+#include "led.h" 
+
+Led my_led (13); //create the LED that is in the built-in pin 13
+
+void setup() {}; //put in here anything that needs to be done on startup
+
+void loop() {
+  my_led.on();  //ask the LED to turn on
+  delay(100);              // wait for a second
+  my_led.off(); //ask the LED to turn off
+  delay(100);              // wait for a second
+}
---
+++ blink/led.h
@@ -0,0 +1,23 @@
+//This file defines the Led class to use
+//Now we can have state and turn the led object on or off, or toggle it if that is preferred
+//include the WProgram.h header file from Arduino so that you get their constants, like OUTPUT, HIGH, LOW
+
+#include "WProgram.h"
+
+class Led {
+  public:
+    int pin_number; //this is an instance variable keeping the pin number for us to use anytime during this object's lifetime
+
+    Led (int n) {      
+      pin_number = n; 
+      pinMode(pin_number, OUTPUT); //set the arduino pin as OUTPUT
+    }
+    
+    void on() {
+      digitalWrite(pin_number, HIGH); //send the 5V to the pin to turn it on
+    }
+    
+    void off() {
+      digitalWrite(pin_number, LOW); //send 0V to the pin to turn it off
+    }
+};
